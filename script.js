5a91effa972b015eb268ba3b2e8c317b068c1450
PriyaSandal <133044444+PriyaSandal@users.noreply.github.com>
2023-07-03 11:19:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function delayedHello() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+delayedHello().then((text) => {
+  document.getElementById("output").innerText = text;
+});
